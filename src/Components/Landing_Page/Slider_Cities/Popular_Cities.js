97e29a27efa9895ee483513ee52e656b7713b702
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-08-31 05:26:25 +0500

    changed

--- src/Components/Landing_Page/Slider_Cities/Popular_Cities.js
+++ src/Components/Landing_Page/Slider_Cities/Popular_Cities.js
@@ -44,7 +44,7 @@ const PopularSelections = () => {
             <div className="flex flex-col items-center justify-center p-4 mt-4">
               <div className="cursor-pointer">
                 <button className="relative inline-flex items-center justify-center px-4 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out bg-white border-2 border-blue-600 rounded-full shadow-md group">
-                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-600 group-hover:translate-x-0 ease">
+                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-800 group-hover:translate-x-0 ease">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                   </span>
                   <span className="absolute flex items-center justify-center w-full h-full text-blue-800 transition-all duration-300 transform group-hover:translate-x-full ease">Explore</span>
@@ -64,7 +64,7 @@ const PopularSelections = () => {
             <div className="flex flex-col items-center justify-center p-4 mt-4">
               <div className="cursor-pointer">
                 <button className="relative inline-flex items-center justify-center px-4 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out bg-white border-2 border-blue-600 rounded-full shadow-md group">
-                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-600 group-hover:translate-x-0 ease">
+                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-800 group-hover:translate-x-0 ease">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                   </span>
                   <span className="absolute flex items-center justify-center w-full h-full text-blue-800 transition-all duration-300 transform group-hover:translate-x-full ease">Continue</span>
