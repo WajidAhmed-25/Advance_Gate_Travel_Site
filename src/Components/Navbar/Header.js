6f88a80b0582632020c196ca4c6f782f52b6a5c5
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-08-31 05:11:38 +0500

    changes

--- src/Components/Navbar/Header.js
+++ src/Components/Navbar/Header.js
@@ -21,8 +21,8 @@ export default function My_Header(){
         
         <header className="flex flex-wrap items-center justify-between px-4 py-4 text-sm bg-gray-100">
   <div className="flex space-x-4">
-    <span className="font-bold text-gray-600">+92 1234567930</span>
-    <span className="hidden font-bold text-gray-600 sm:inline">+92 1234567930</span>
+    <span className="font-semibold text-gray-600">+92 1234567930</span>
+    <span className="hidden font-semibold text-gray-600 sm:inline">+92 1234567930</span>
   </div>
   <div className="flex items-center space-x-4">
     <FaTelegram className="w-5 h-5 text-blue-400" />
