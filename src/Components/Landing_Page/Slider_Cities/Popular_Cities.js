01a277486d4c845caae011aa5ef25391eb533c17
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-08-31 05:23:48 +0500

    data_change

--- src/Components/Landing_Page/Slider_Cities/Popular_Cities.js
+++ src/Components/Landing_Page/Slider_Cities/Popular_Cities.js
@@ -47,7 +47,7 @@ const PopularSelections = () => {
                   <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-600 group-hover:translate-x-0 ease">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                   </span>
-                  <span className="absolute flex items-center justify-center w-full h-full text-blue-600 transition-all duration-300 transform group-hover:translate-x-full ease">Explore</span>
+                  <span className="absolute flex items-center justify-center w-full h-full text-blue-800 transition-all duration-300 transform group-hover:translate-x-full ease">Explore</span>
                   <span className="relative invisible">Continue</span> 
                 </button>
               </div>
@@ -67,7 +67,7 @@ const PopularSelections = () => {
                   <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-blue-600 group-hover:translate-x-0 ease">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                   </span>
-                  <span className="absolute flex items-center justify-center w-full h-full text-blue-600 transition-all duration-300 transform group-hover:translate-x-full ease">Continue</span>
+                  <span className="absolute flex items-center justify-center w-full h-full text-blue-800 transition-all duration-300 transform group-hover:translate-x-full ease">Continue</span>
                   <span className="relative invisible">Explore</span>
                 </button>
               </div>
